c6cb15f20ea4904ab499cb5946c54b2e832c126e
Andrew Garcia <andrewagarcia8@gmail.com>
2022-04-27 17:35:26 -0700

    changes to heroku

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -89,7 +89,6 @@ router.get('/:id', (req, res) => {
 
  // create a post
  router.post('/', (req, res) => {
-  // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
     title: req.body.title,
     post_content: req.body.post_content,
